037c3cd33c43c8c5934e9e9b091aa785927b0825
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2021-10-24 21:15:53 -0700

    add port parameter

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
+const PORT = Deno.env.get("PORT") || "8000";
+
 function handler(req) {
     return new Response("Hello world");
 }
 
-console.log("Listening on http://localhost:8000");
-await listenAndServe(":8000", handler);
+console.log(`Listening on http://localhost:${PORT}`);
+await listenAndServe(`:${PORT}`, handler);
