87918e7a05d3d0dad4cce42408f8c98ba03687a8
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2021-10-24 21:17:15 -0700

    add new message

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 const PORT = Deno.env.get("PORT") || "8000";
 
 function handler(req) {
-    return new Response("Hello world");
+    return new Response("Hello world from deno");
 }
 
 console.log(`Listening on http://localhost:${PORT}`);
